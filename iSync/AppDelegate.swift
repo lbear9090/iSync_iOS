1533ed73fd11ce43790fd22c121be93b30e7c84e
Lucky <lbear9090@hotmail.com>
2021-06-08 06:35:08 -0700

    Initial Commit

---
+++ iSync/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  iSync
+//
+//  Created by Lucky on 05/31/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ iSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iSync
+//
+//  Created by Lucky on 05/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
